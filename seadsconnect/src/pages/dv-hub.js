b58572414897d59199077aff4829b8a20f38134b
s2eagle <jhui7@ucsc.edu>
2019-03-18 01:43:35 -0700

    Added live updating graph. Removed unused washer button that was used for testing

--- seadsconnect/src/pages/dv-hub.js
+++ seadsconnect/src/pages/dv-hub.js
@@ -7,112 +7,6 @@ import Appliances from "../Graphs/DragGraph/appliances"
 
 var d3 = require("d3");
 
-	/*
-	This stuff is a something I was playing around with to
-	get used to d3. Since I can't get my line graph to work,
-	I'm uploading this temporary bar graph which I had working
-	which doesn't require a csv and only the hourData array.
-
-	var hourData = [500,300,100,100,100,100,100,100,400,800,1000,1500,2000,2300,2000,1800,1800,2000,2400,2600,3000,2500,1500,1000,200];
-
-	var mainWidth = 1400
-	var mainHeight = 800
-	var barWidth = 49
-	  d3.select("body").append("svg")
-		.attr("width", mainWidth)
-		.attr("height", mainHeight)
-		.style("background", "#b0b0b0")
-		  .selectAll('rect')
-		    .data(hourData)
-			.enter().append('rect')
-			  .attr("width", barWidth)
-			  .style("background","#00ff00")
-			  .attr("height", function(d) { //d = data
-			    return d/5;
-			  })
-			  .attr("x", function(d, i) { //i = index
-			    return i * (barWidth+1);
-			  })
-			  .attr("y", function(d) {
-			    return mainHeight - d/5;
-			  })
-
-	/* some of the things i've tried to get csv import to work
-	var workingDir = window.location.pathname.split('/').slice(0, -1).join('/')
-	var datafile = workingDir + "/secondData.csv";
-	console.log(workingDir);
-	console.log(datafile);
-	*/
-
-	/*
-
-
-		var workingDir = window.location.pathname.split('/').slice(0, -1).join('/')
-	var datafile = workingDir + "/secondData.csv";
-	d3.csv(datafile, function (error, data) {
-	  if (error) throw error;
-
-	  data.forEach(function(d) {
-	    console.log(d);
-		d.Second = +d.Second; // this needs to be done to change value to int
-		d.Energy = +d.Energy; // Otherwise, csv is interpreted as a string
-	  });
-
-	  var width = 1000;
-	  var height = 800;
-	  var margin = {left: 50, right: 60, top: 30, bottom: 60};
-
-	  var x = d3.scaleLinear().range([0, width]);
-	  var y = d3.scaleLinear().range([height, 0]);
-
-	  x.domain([d3.min(data, function(d) {return d.Second;})
-			,d3.max(data, function(d) {return d.Second;})]);
-	  y.domain([0,d3.max(data, function(d) {return d.Energy;})]);
-
-
-	  var areaFill = d3.area()
-	    .x(function(d) { return x(d.Second); })
-	    .y(function(d) { return y(d.Energy); })
-	    .y1(height);
-
-	  var svg = d3.select("body").append("svg")
-	    .attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	    .append("g")
-		  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	  svg.append("path")
-	    .data([data])
-	    .attr("fill", "#ffb2b2")
-	    .attr("class", "line")
-	    .attr("d", areaFill)
-	    .attr("stroke", "#ff0000")
-		.attr("stroke-width", "2px")
-		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-	  svg.append("g")
-		.attr("transform", "translate("+ margin.left + "," + (height + margin.top) + ")") //20 for text size
-	    .call(d3.axisBottom(x));
-
-	  svg.append("text")
-	    .attr("x", margin.left+width/2)
-		.attr("y", height+margin.top+(margin.bottom)/2)
-		.style("text-anchor", "middle")
-		.text("Second");
-
-	  svg.append("g")
-	    .call(d3.axisLeft(y))
-		.attr("transform", "translate(" + margin.left + "," + 0 + ")");
-
-	  svg.append("text")
-	    .attr("x", 0)
-		.attr("y", margin.top+height/2)
-		.attr("text-anchor", "middle")
-		.text("Energy");
-	});
-	*/
-	
-	
 //Thresholdbar: Change pass watts into value and change max to what ever you want.
 class DvHub extends Component {
 	constructor(props) {
@@ -122,6 +16,7 @@ class DvHub extends Component {
 				m: 100,
 				thresh: 33,
 				liveData: 0, 
+				savedData: [],
 				liveTime: new Date(0).toLocaleString(),
 				washerToggleOn: true,
 				dryerToggleOn: true,
@@ -216,6 +111,7 @@ class DvHub extends Component {
 
 	componentDidMount() {
         this.interval = setInterval(() => this.updatePower(), 500);
+		setInterval(() => this.drawChart(), 1000);
     }
 
 
@@ -225,12 +121,88 @@ class DvHub extends Component {
 	
 	updatePower = () => {
 		var liveUpdateURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
-		d3.json(liveUpdateURL).then( (liveDataa) => {
-			var watts = liveDataa.DataPoints[0].Power;
+		d3.json(liveUpdateURL).then( (liveData) => {
+			var watts = liveData.DataPoints[0].Power;
+			/* The current time isn't pulling the time from the seads device.
+			 * Is this intended?
+			 */
 			var currentTime = new Date().toLocaleString();
 			this.setState({liveData: watts });	
 			this.setState({liveTime: currentTime });
 		});
+	}
+	
+	drawChart = () => {
+		var width = 1000;
+		var height = 400;
+		var margin = {left: 80, right: 60, top:30, bottom:60};
+		
+		var x = d3.scaleLinear().range([0, width]);
+		var y = d3.scaleLinear().range([height, 0]);
+		var mode = "random";
+		
+		var currentData = this.state.savedData;
+		
+		/* mode 1: random data  between 1 and 101 */
+		if (mode == "random") {
+			currentData[currentData.length]={"Second":currentData.length+1, "Energy":(Math.floor(Math.random()*100)+1)};
+		}
+		
+		/* mode 2: based on live data */
+		/* TODO: This assumes that data comes in consistently at once 
+		 * every second. This will not always be the case in real life.
+		 */
+		if (mode == "live") {
+			currentData[currentData.length]={"Second":currentData.length+1, "Energy":this.state.liveData};
+		}
+		
+		this.setState({savedData : currentData});
+		  
+		x.domain([d3.min(currentData, function(d) {return d.Second;})
+		         ,d3.max(currentData, function(d) {return d.Second;})]);
+		y.domain([0,d3.max(currentData, function(d) {return d.Energy;})]);
+		
+		/* This is used to remove the current graph before replacing with updated graph */
+		d3.selectAll("svg").remove()
+		  
+		var areaFill = d3.area()
+		  .x(function(d) { return x(d.Second); })
+		  .y(function(d) { return y(d.Energy); })
+		  .y1(height);
+		
+		var svg = d3.select("body")
+		  .append("svg")
+		    .attr("width", width + margin.left + margin.right)
+		    .attr("height", height + margin.left + margin.right)
+					  
+		svg.append("path")
+		  .data([currentData])
+			.attr("fill", "#ffb2b2")
+			.attr("class", "line")
+			.attr("d", areaFill)
+			.attr("stroke", "#ff0000")
+			.attr("stroke-width", "2px")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			
+		svg.append("g")
+		  .attr("transform", "translate(" + margin.left + "," + (margin.top+height) + ")")
+		    .call(d3.axisBottom(x));
+		  
+		svg.append("g")
+		  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		  .call(d3.axisLeft(y));
+		  
+		svg.append("text")
+		  .attr("x", margin.left+width/2)
+		  .attr("y", height+margin.top+margin.bottom/2)
+		    .style("text-anchor", "middle")
+		      .text("Time (in second)");
+		  
+		svg.append("text")
+		  .attr("x", 0)
+		  .attr("y", margin.top+height/2)
+		  .attr("text-anchor", "left")
+		    .text("Watts");
 	}
 
 	render() {
@@ -242,14 +214,13 @@ class DvHub extends Component {
 		let computerColor = this.state.computerToggleOn ? "outline-success" : "success";
 		
 		return (
-			<Layout>
-				<h1>current power: { this.state.liveData }</h1>
-				<h2>current date: { this.state.liveTime }</h2>
+		<Layout>
+			<h1>current power: { this.state.liveData }</h1>
+			<h2>current date: { this.state.liveTime }</h2>
 			<Thresholdbar value={this.state.val + this.state.liveData} max={this.state.m} thresholds={this.state.thresh} />
 				<div align="center">
 					<ButtonGroup>
 						<ButtonToolbar>
-							<Button variant={washerColor} onClick={() => {this.updatePower()}} >Washer</Button>
 							<Button variant={washerColor} onClick={() => {this.toggleWasher(); this.changeColorWasher()}} >Washer</Button>
 							<Button variant={dryerColor} onClick={() => {this.toggleDryer(); this.changeColorDryer()}} >Dryer</Button>
 							<Button variant={ovenColor} onClick={() => {this.toggleOven(); this.changeColorOven()}} >Oven</Button>
@@ -259,6 +230,7 @@ class DvHub extends Component {
 						</ButtonToolbar>
 					</ButtonGroup>
 				</div>
+				<div id={"#" + this.props.id}></div>
 		</Layout>
 		)
 	}
