02580d3cb640bb9c6a4e1be59217774951b5a6ef
Austin Harmon <35510464+apharmon2112@users.noreply.github.com>
2019-04-15 12:56:57 -0700

    Contained device data polling into singular function

--- seadsconnect/src/components/Profile/getDeviceID.js
+++ seadsconnect/src/components/Profile/getDeviceID.js
@@ -11,21 +11,19 @@ class GetDevice {
         liveData: 0,
         liveTime: new Date().toLocaleString(),
       }
-      this.mount = true
     }
 
-    async findID() {
-      console.log("In findIDr");
+    async findID(userID) {
       if( !getFirebase().auth().currentUser) {
         return 'TEST';
       }
       else {
-        console.log("logged in");
-        var userId = getFirebase().auth().currentUser.uid;
-        var ref = getFirebase().database().ref('users/' + userId + '/seadsDevice/seadsID' );
+        var ref = getFirebase().database().ref('users/' + userID + '/seadsDevice/seadsID' );
         return ref.once("value").then(function(snapshot) {
           if(snapshot.exists()) {
-            console.log("found snapshot");
+            if(snapshot.val() === ''){
+              return -1;
+            }
             return snapshot.val();
           }
         });
@@ -33,43 +31,61 @@ class GetDevice {
     }
 
     async findIndex(id) {
-      console.log("In findIndex");
+      if(id < 0){
+        return -1;
+      }
       if( !getFirebase().auth().currentUser){
         return 0;
       }
       else{
-        console.log("logged in");
         var seadsURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
         return d3.json(seadsURL).then( (liveData) => {
           var i;
-          console.log(id + "ID in index loop");
           for(i = 0; i < liveData.DataPoints.length; i++){
             if(liveData.DataPoints[i].DeviceId == id){
-              console.log(i + " In index loop");
               return i;
             }
           }
+          //Returns -1 when the user has a SEADS ID, but it is not in the Database
+          return -2;
         });
       }
     }
 
     async findPower(id) {
-      console.log("In findPower");
-      console.log(id);
+
         var seadsURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
         return d3.json(seadsURL).then( (liveData) => {
-          console.log(id + " ID from .json");
           if(id >= 0){
-            console.log("searching DB");
             var watts = liveData.DataPoints[id].Power;
             var currentTime = new Date(liveData.DataPoints[id].Timestamp*1000).toLocaleString();
             this.liveData = watts;
             this.liveTime = currentTime;
-            console.log(this.liveData);
-            console.log(this.liveTime);
             return [watts, currentTime];
           }
         });
+    }
+
+    async getSeadsData(userID) {
+      return this.findID(userID).then( (id) => {
+          this.userDeviceId = id;
+          return this.findIndex(this.userDeviceId).then( (ind) => {
+              this.userDeviceIndex = ind;
+              if(this.userDeviceIndex === -1){
+                return ["-1", "-1"];
+              }
+              else if(this.userDeviceIndex === -2){
+                return ['-2', '-2']
+              }
+              return this.findPower(this.userDeviceIndex).then((powTime) => {
+                if(powTime){
+                  this.liveData = powTime[0];
+                  this.liveTime = powTime[1];
+                }
+                return [this.liveData, this.liveTime];
+              });
+          });
+      });
     }
 }
 export default GetDevice;
