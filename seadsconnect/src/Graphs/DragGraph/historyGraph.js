238b02ebecfd27d9481740fc600a99bd4ede5c7b
Austin Harmon <35510464+apharmon2112@users.noreply.github.com>
2019-04-24 14:45:40 -0700

    Readied graph for SEADS DB API calls

--- seadsconnect/src/Graphs/DragGraph/historyGraph.js
+++ seadsconnect/src/Graphs/DragGraph/historyGraph.js
@@ -20,21 +20,34 @@ export default class historyGraph() extends Component{
     ];
 
     var svgWidth = 500, svgHeight = 400;
-    var margin = 10;
-    var width = svgWidth - margin;
-    var height = svgHeight - margin;
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = svgWidth - margin.left - margin.right,
+        height = svgHeight - margin.top - margin.bottom;
 
     var svg = d3.select('svg')
       .attr("width", svgWidth)
       .attr("height", svgHeight);
 
       var g = svg.append("g")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
       var x = d3.scaleTime().rangeRound([0, width]);
       var y = d3.scaleLinear().rangeRound([height, 0]);
 
-      var line = d3.line();
+      var lineLastWeek = d3.line()
+        .x(function(d) { return x(d.day); })
+        .y(function(d) { return y(d.powers); });
+
+      var lineThisWeek = d3.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.open); });
+
+
+      x.domain(testData.map(function(d) { return d.day; }));
+      y.domain(testData.map(function(d) { return d.power; }));
 
 
   }
