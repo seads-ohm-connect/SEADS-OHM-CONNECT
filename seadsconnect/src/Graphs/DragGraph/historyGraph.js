94dfd9e7c9460640b1869b9fdcab0300762ff4c2
Austin Harmon <35510464+apharmon2112@users.noreply.github.com>
2019-05-01 13:23:57 -0700

    Added keys.js. commiting to switch to OhmConnect work

--- seadsconnect/src/Graphs/DragGraph/historyGraph.js
+++ seadsconnect/src/Graphs/DragGraph/historyGraph.js
@@ -7,47 +7,43 @@ var d3 = require("d3");
 export default class historyGraph() extends Component{
 
 
-  function drawChart (data) {
+  var svgWidth = 500, svgHeight = 400;
+  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = svgWidth - margin.left - margin.right,
+      height = svgHeight - margin.top - margin.bottom;
 
-    const testData = [
-      {day: "Monday", power: 10},
-      {day: "Tuesday", power: 30},
-      {day: "Wednesday", power: 50},
-      {day: "Thursday", power: 20},
-      {day: "Friday", power: 40},
-      {day: "Saturday", power: 70},
-      {day: "Sunday", power: 60}
-    ];
+  var svg = d3.select('svg')
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-    var svgWidth = 500, svgHeight = 400;
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = svgWidth - margin.left - margin.right,
-        height = svgHeight - margin.top - margin.bottom;
+    var g = svg.append("g")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var svg = d3.select('svg')
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
+    var x = d3.scaleTime().rangeRound([0, width]);
+    var y = d3.scaleLinear().rangeRound([height, 0]);
 
-      var g = svg.append("g")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var lineLastWeek = d3.line()
+      .x(function(d) { return x(d.day); })
+      .y(function(d) { return y(d.power); });
 
-      var x = d3.scaleTime().rangeRound([0, width]);
-      var y = d3.scaleLinear().rangeRound([height, 0]);
+    var lineThisWeek = d3.line()
+      .x(function(d) { return x(d.day); })
+      .y(function(d) { return y(d.power; });
 
-      var lineLastWeek = d3.line()
-        .x(function(d) { return x(d.day); })
-        .y(function(d) { return y(d.powers); });
+    function draw(data, device) {
 
-      var lineThisWeek = d3.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.open); });
+      data.forEach(function(d) {
 
 
+      })
+      d3.json()
       x.domain(testData.map(function(d) { return d.day; }));
       y.domain(testData.map(function(d) { return d.power; }));
+
+    }
 
 
   }
