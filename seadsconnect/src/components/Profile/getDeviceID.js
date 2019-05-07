858f099c6e508b774598970384ffe171fe3400aa
AriBerkson <aberkson38@gmail.com>
2019-05-07 14:44:15 -0700

    fixed indexing problem

--- seadsconnect/src/components/Profile/getDeviceID.js
+++ seadsconnect/src/components/Profile/getDeviceID.js
@@ -45,7 +45,7 @@ class GetDevice {
     //Helper function that takes the id found from the fidID function
     //And get the index in which the SEADS device is in the SEADS server list
     //This function MUST be called after findID
-    async findIndex(id) {
+    async findIndex(id, liveData) {
       if(id < 0){
         return -1;
       }
@@ -53,8 +53,6 @@ class GetDevice {
         return 0;
       }
       else{
-        var seadsURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
-        return d3.json(seadsURL).then( (liveData) => {
           var i;
           for(i = 0; i < liveData.DataPoints.length; i++){
             if(liveData.DataPoints[i].DeviceId == id){
@@ -63,25 +61,21 @@ class GetDevice {
           }
           //Returns -1 when the user has a SEADS ID, but it is not in the Database
           return -2;
-        });
       }
     }
 
     //Helper function that uses the index retrieved from findIndex and pulls
     //the live data and time from the SEADS server
     //This function must be called after findIndex
-    async findPower(id) {
+    async findPower(id, liveData) {
 
-        var seadsURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
-        return d3.json(seadsURL).then( (liveData) => {
-          if(id >= 0){
-            var watts = liveData.DataPoints[id].Power;
-            var currentTime = new Date(liveData.DataPoints[id].Timestamp*1000).toLocaleString();
-            this.liveData = watts;
-            this.liveTime = currentTime;
-            return [watts, currentTime];
-          }
-        });
+        if(id >= 0){
+          var watts = liveData.DataPoints[id].Power;
+          var currentTime = new Date(liveData.DataPoints[id].Timestamp*1000).toLocaleString();
+          this.liveData = watts;
+          this.liveTime = currentTime;
+          return [watts, currentTime];
+        }
     }
 
     //Main getter method that makes a function call list using findID, findIndex, and findPower
@@ -91,9 +85,11 @@ class GetDevice {
     //the function(s) that are in side it.
     //Each return statement sends the value from the function up to the function that its nested in
     async getSeadsData(userID) {
-      return this.findID(userID).then( (id) => {
+      var seadsURL = new String("http://seadsone.soe.ucsc.edu:8000/api/seads/power/last");
+      return d3.json(seadsURL).then((liveData) => {
+        return this.findID(userID).then( (id) => {
           this.userDeviceId = id;
-          return this.findIndex(this.userDeviceId).then( (ind) => {
+          return this.findIndex(this.userDeviceId, liveData).then( (ind) => {
               this.userDeviceIndex = ind;
               if(this.userDeviceIndex === -1){
                 return ["-1", "-1"];
@@ -101,7 +97,7 @@ class GetDevice {
               else if(this.userDeviceIndex === -2){
                 return ['-2', '-2']
               }
-              return this.findPower(this.userDeviceIndex).then((powTime) => {
+              return this.findPower(this.userDeviceIndex, liveData).then((powTime) => {
                 if(powTime){
                   this.liveData = powTime[0];
                   this.liveTime = powTime[1];
@@ -109,6 +105,7 @@ class GetDevice {
                 return [this.liveData, this.liveTime];
               });
           });
+        });
       });
     }
 
