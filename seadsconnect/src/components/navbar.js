2cbedb540785af2f8a2ab684c24dcb4fce9bb840
AriBerkson <aberkson38@gmail.com>
2019-06-10 13:12:43 -0700

    fixed bugs

--- seadsconnect/src/components/navbar.js
+++ seadsconnect/src/components/navbar.js
@@ -4,19 +4,6 @@ import { Navbar, Nav, Button} from "react-bootstrap"
 import getFirebase from "../components/firebase"
 import SEADSConnectLogo from "../images/SEADSConnectLogo.png"
 
-//
-// export default () => (
-//
-//   <Navbar bg="light" varient="light" sticky="top">
-//     <Navbar.Brand href="/">SEADSConnect</Navbar.Brand>
-//     <Nav classname="bar">
-//       <Nav.Link href="/dv-hub/">Metrics</Nav.Link>
-//       <Nav.Link href="/page-2/">Sign In</Nav.Link>
-//       <Nav.Link href="/page-3/">Sign Up</Nav.Link>
-//     </Nav>
-//   </Navbar>
-// )
-
 
 export default () => (
     <NavBarHandler />
@@ -26,59 +13,24 @@ class NavBarHandler extends Component {
   constructor(props) {
       super(props);
 
-      this.state = {
-        signedIn : getFirebase().auth().currentUser
-      }
   }
 
 
   toSignOut = () => {
-      if (this.state.signedIn) {
+      if (this.getSignedIn()) {
         getFirebase().auth().signOut().then(function() {
           sessionStorage.removeItem("signedIn");
         })
       }
   }
 
-  getData() {
-    setTimeout(() => {
-      this.setState({
-        signedIn: this.getSignedIn()
-      })
-    })
-  }
-
   getSignedIn() {
     const cachedHits = sessionStorage.getItem("signedIn");
-    if (cachedHits) {
-      return true;
-    }
-    else 
-      return false;
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  isSignedIn() {
-    if (!this.state.signedIn) 
-      this.state.signedIn = this.getSignedIn();
-
-    return this.getSignedIn() ? <a class="nav-link active" href="/">Sign Out</a> :
-                                 <a class="nav-link active" href="/page-2/">Sign In</a>;
-  }
-
-  getProfile() {
-    if (!this.state.signedIn) 
-      this.state.signedIn = this.getSignedIn();
-
-    return this.getSignedIn() ? <a class="nav-link active" href="/profile">Profile</a> :
-                                 <a class="nav-link active" href="/page-3/">Sign Up</a>;
+    return cachedHits;
   }
 
   render() {
-      this.state.signedIn = this.getSignedIn();
+
       return (
 
        <Navbar bg="dark" variant="dark" fixed="top">
@@ -100,37 +52,14 @@ class NavBarHandler extends Component {
           <Nav.Link href="/training-module/">Training</Nav.Link>
         </Nav>
         <Nav>
-          <Nav.Link onClick={this.toSignOut}>{this.isSignedIn()}</Nav.Link>
-          <Nav.Link onClick={this.getProfile}>{this.getProfile()} </Nav.Link>
+          {this.getSignedIn() ? <Nav.Link href="/" onClick={this.toSignOut}>Sign Out</Nav.Link> :
+                                                              <Nav.Link href="/page-2/">Sign In</Nav.Link>};
+
+          {this.getSignedIn() ? <Nav.Link href="/profile">Profile</Nav.Link> :
+                                    <Nav.Link href="/page-3/">Sign Up</Nav.Link>};
         </Nav>
       </Navbar> 
-
-      // <nav class="navbar navbar-expand-sm navbar-light bg-light">
-      //     <div class="navbar-collapse order-1 dual-collapse2">
-      //         <ul class="navbar-nav">
-      //             <li class="nav-item active">
-      //                 <a class="nav-link" href="/">SEADSConnect</a>
-      //             </li>
-      //             <li class="nav-item active">
-      //                 <a class="nav-link" href="/dv-hub/">Metrics</a>
-      //             </li>
-      //             <li class="nav-item active">
-      //                 <a class="nav-link" href="/training-module/">Training</a>
-      //             </li>                  
-      //         </ul>
-      //     </div>
-      //     <div class="navbar-collaps order-3 dual-collapse2">
-      //         <ul class="navbar-nav">
-      //             <li class="nav-item" onClick={this.toSignOut} > 
-      //                 {this.isSignedIn()}
-      //             </li>
-      //             <li class="nav-item active">
-      //                 {this.getProfile()}
-      //             </li>
-      //         </ul>
-      //     </div>
-      //   </nav>
        
-      )
-    }
+      );
+  }
 }
